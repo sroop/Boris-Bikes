20245593b24333fc0c9c640ecbabe12009a6f6a2
Sroop <sroopsunar@hotmail.com>
2014-04-15 16:19:44 +0100

    Docking Station - changed release bike method into two methods, release working bikes and release broken bikes

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,19 +4,29 @@ describe "Docking Station" do
  	let(:bike) {double :bike}
  	let(:station) { DockingStation.new(capacity: 20)}
 
-it 'should accept a bike' do
+it 'should accept all bikes' do
 	expect(station.bike_count).to eq(0)
 	station.dock(bike)
 	expect(station.bike_count).to eq(1)
 end
 
-it 'should release a bike' do
-	station.dock(bike)
-	station.release(bike)
+it 'should release a broken bike' do
+	broken_bike = double :bike, broken?: true
+	station.dock(broken_bike)
+	expect(station.bike_count).to eq(1)
+	station.release_broken_bikes
 	expect(station.bike_count).to eq(0)
 end
 
-it "should know when it's full" do
+it 'should release a working bike' do
+	working_bike = double :bike, broken?: false
+	station.dock(working_bike)
+	expect(station.bike_count).to eq(1)
+	station.release_working_bikes
+	expect(station.bike_count).to eq(0)
+end
+
+it "should know when it's reached full capacity" do
 	expect(station).not_to be_full
 	20.times { station.dock(bike) }
 	expect(station).to be_full
@@ -27,5 +37,20 @@ it "should not accept a bike if it's full" do
 	expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
 end
 
+it 'should provide a list of working bikes' do
+	working_bike = double :bike, broken?: false
+	broken_bike = double :bike, broken?: true
+	station.dock(working_bike)
+	station.dock(broken_bike)
+	expect(station.working_bikes).to eq([working_bike])
+end
+
+it 'should provide a list of broken bikes' do
+	working_bike = double :bike, broken?: false
+	broken_bike = double :bike, broken?: true
+	station.dock(working_bike)
+	station.dock(broken_bike)
+	expect(station.broken_bikes).to eq([broken_bike])
+end
 
 end
