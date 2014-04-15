e177bc781f2d70c1c2ec50ef4e52152b98df5736
Sroop <sroopsunar@hotmail.com>
2014-04-15 16:29:30 +0100

    Docking Station- changed method names to accept and release for consistency across all classes for clarity

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -10,7 +10,7 @@ class DockingStation
 		@bikes.count
 	end 
 
-	def dock(bike)
+	def accept(bike)
 		raise "Station is full" if full?
 		@bikes << bike
 	end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -6,13 +6,13 @@ describe "Docking Station" do
 
 it 'should accept all bikes' do
 	expect(station.bike_count).to eq(0)
-	station.dock(bike)
+	station.accept(bike)
 	expect(station.bike_count).to eq(1)
 end
 
 it 'should release a broken bike' do
 	broken_bike = double :bike, broken?: true
-	station.dock(broken_bike)
+	station.accept(broken_bike)
 	expect(station.bike_count).to eq(1)
 	station.release_broken_bikes
 	expect(station.bike_count).to eq(0)
@@ -20,7 +20,7 @@ end
 
 it 'should release a working bike' do
 	working_bike = double :bike, broken?: false
-	station.dock(working_bike)
+	station.accept(working_bike)
 	expect(station.bike_count).to eq(1)
 	station.release_working_bikes
 	expect(station.bike_count).to eq(0)
@@ -28,28 +28,28 @@ end
 
 it "should know when it's reached full capacity" do
 	expect(station).not_to be_full
-	20.times { station.dock(bike) }
+	20.times { station.accept(bike) }
 	expect(station).to be_full
 end
 
 it "should not accept a bike if it's full" do
-	20.times { station.dock(bike) }
-	expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
+	20.times { station.accept(bike) }
+	expect(lambda { station.accept(bike) }).to raise_error(RuntimeError)
 end
 
 it 'should provide a list of working bikes' do
 	working_bike = double :bike, broken?: false
 	broken_bike = double :bike, broken?: true
-	station.dock(working_bike)
-	station.dock(broken_bike)
+	station.accept(working_bike)
+	station.accept(broken_bike)
 	expect(station.working_bikes).to eq([working_bike])
 end
 
 it 'should provide a list of broken bikes' do
 	working_bike = double :bike, broken?: false
 	broken_bike = double :bike, broken?: true
-	station.dock(working_bike)
-	station.dock(broken_bike)
+	station.accept(working_bike)
+	station.accept(broken_bike)
 	expect(station.broken_bikes).to eq([broken_bike])
 end
 
