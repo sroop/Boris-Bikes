d6ac89f640bfaba28cc3240851cb58a48301d33d
Sroop <sroopsunar@hotmail.com>
2014-04-15 23:25:34 +0100

    BikeContainer has a maximum capacity defined. error is raised when it hits full capacity

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -1,15 +1,26 @@
 module BikeContainer
 
+	DEFAULT_CAPACITY = 20
+
 	def bikes
 		@bikes ||= []
 	end
 
+	def capacity
+		@capacity ||= DEFAULT_CAPACITY
+	end
+
 	def accept(bike)
+		raise "Full up! No more space!" if full?
 		bikes << bike
 	end
 
 	def release(bike)
 		bikes.select { |bike| bikes.delete(bike)}
+	end
+
+	def full?
+		bikes.count == capacity
 	end
 
 end
--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -10,17 +10,30 @@ describe "Bike Container" do
 	let(:container) {DummyContainer.new}
 
 	it 'accepts bikes' do
-	expect(container.bikes.count).to eq(0)
-	container.accept(bike)
-	expect(container.bikes.count).to eq(1)
+		expect(container.bikes.count).to eq(0)
+		container.accept(bike)
+		expect(container.bikes.count).to eq(1)
 	end
 
 	it 'releases bikes' do
-	expect(container.bikes.count).to eq(0)
-	container.accept(bike)
-	expect(container.bikes.count).to eq(1)
-	container.release(bike)
-	expect(container.bikes.count).to eq(0)
+		expect(container.bikes.count).to eq(0)
+		container.accept(bike)
+		expect(container.bikes.count).to eq(1)
+		container.release(bike)
+		expect(container.bikes.count).to eq(0)
 	end
+
+	it 'can reach full capacity' do
+		expect(container).not_to be_full
+		20.times { container.accept(bike) }
+		expect(container).to be_full
+	end
+
+	it 'raises an error when it reaches full capacity' do
+		expect(container.bikes.count).to eq(0)
+		20.times { container.accept(bike) }
+		expect(lambda { container.accept(bike) }).to raise_error(RuntimeError)
+	end
+
 
 end
