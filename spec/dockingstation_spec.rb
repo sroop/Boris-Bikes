5e158ea701b8147ffdbba0a6faaba5ad318b0870
Sroop <sroopsunar@hotmail.com>
2014-04-16 01:27:51 +0100

    DockingStation - amended the test, commented out most of it because including BikeContainer module which has its own spec sheet, allowed a couple tests to double check all is ok

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,51 +2,8 @@ require 'dockingstation'
 
 describe "Docking Station" do
 
-	let(:bike) {double :bike}
-	let(:broken_bike) {double :bike, broken?: true}
-	let(:working_bike) {double :bike, broken?: false}
-	let(:container) {DummyContainer.new}
-
-	it 'accepts bikes' do
-		expect(container.bikes.count).to eq(0)
-		container.accept(bike)
-		expect(container.bikes.count).to eq(1)
-	end
-
-	it 'releases bikes' do
-		expect(container.bikes.count).to eq(0)
-		container.accept(bike)
-		expect(container.bikes.count).to eq(1)
-		container.release(bike)
-		expect(container.bikes.count).to eq(0)
-	end
-
-	it 'can reach full capacity' do
-		expect(container).not_to be_full
-		20.times { container.accept(bike) }
-		expect(container).to be_full
-	end
-
-	it 'raises an error when it reaches full capacity' do
-		expect(container.bikes.count).to eq(0)
-		20.times { container.accept(bike) }
-		expect(lambda { container.accept(bike) }).to raise_error(RuntimeError)
-	end
-
-	it 'segregates broken bikes' do
-		container.accept(broken_bike)
-		container.accept(working_bike)
-		expect(container.broken_bikes).to eq([broken_bike])
-	end
-
-	it 'segregates working bikes' do
-		container.accept(broken_bike)
-		container.accept(working_bike)
-		expect(container.working_bikes).to eq([working_bike])
-	end
-
-#  	let(:bike) {double :bike}
-#  	let(:station) { DockingStation.new(capacity: 20)}
+ 	let(:bike) {double :bike}
+ 	let(:station) { DockingStation.new(capacity: 20) }
 
 # it 'should accept all bikes' do
 # 	expect(station.bike.count).to eq(0)
@@ -76,10 +33,10 @@ describe "Docking Station" do
 # 	expect(station).to be_full
 # end
 
-# it "should not accept a bike if it's reached full capacity" do
-# 	20.times { station.accept(bike) }
-# 	expect(lambda { station.accept(bike) }).to raise_error(RuntimeError)
-# end
+it "should not accept a bike if it's reached full capacity" do
+	20.times { station.accept(bike) }
+	expect(lambda { station.accept(bike) }).to raise_error(RuntimeError)
+end
 
 # it 'should provide a list of working bikes' do
 # 	working_bike = double :bike, broken?: false
@@ -89,12 +46,12 @@ describe "Docking Station" do
 # 	expect(station.working_bikes).to eq([working_bike])
 # end
 
-# it 'should provide a list of broken bikes' do
-# 	working_bike = double :bike, broken?: false
-# 	broken_bike = double :bike, broken?: true
-# 	station.accept(working_bike)
-# 	station.accept(broken_bike)
-# 	expect(station.broken_bikes).to eq([broken_bike])
-# end
+it 'should provide a list of broken bikes' do
+	working_bike = double :bike, broken?: false
+	broken_bike = double :bike, broken?: true
+	station.accept(working_bike)
+	station.accept(broken_bike)
+	expect(station.broken_bikes).to eq([broken_bike])
+end
 
 end
