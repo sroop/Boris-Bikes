aed50c0661463e809bf0ec7c7a28893b4355df70
Sroop <sroopsunar@hotmail.com>
2014-04-16 01:08:23 +0100

    DockingStation - included BikeContainer, commented out old code. Created alias names dock and rent for accept and release

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,4 +1,9 @@
+require_relative 'bike_container'
+
 class DockingStation
+
+	include BikeContainer
+
 	DEFAULT_CAPACITY = 10
 
 	def initialize(options = {})
@@ -6,33 +11,36 @@ class DockingStation
 		@bikes = []
 	end
 
-	def bike_count
-		@bikes.count
-	end 
+alias :dock :accept
+alias :rent :release
 
-	def accept(bike)
-		raise "Station is full" if full?
-		@bikes << bike
-	end
+	# def bike_count
+	# 	@bikes.count
+	# end 
 
-	def full?
-		bike_count == @capacity
-	end
+	# def accept(bike)
+	# 	raise "Station is full" if full?
+	# 	@bikes << bike
+	# end
 
-	def working_bikes
-		@bikes.reject { |bike| bike.broken? }
-	end
+	# def full?
+	# 	bike_count == @capacity
+	# end
 
-	def broken_bikes
-		@bikes.select { |bike| bike.broken? }
-	end
+	# def working_bikes
+	# 	@bikes.reject { |bike| bike.broken? }
+	# end
 
-	def release_broken_bikes
-		broken_bikes.map { |bike| @bikes.delete(bike) }
-	end
+	# def broken_bikes
+	# 	@bikes.select { |bike| bike.broken? }
+	# end
 
-	def release_working_bikes
-		working_bikes.map { |bike| @bikes.delete(bike) }
-	end
+	# def release_broken_bikes
+	# 	broken_bikes.map { |bike| @bikes.delete(bike) }
+	# end
+
+	# def release_working_bikes
+	# 	working_bikes.map { |bike| @bikes.delete(bike) }
+	# end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,56 +1,100 @@
 require 'dockingstation'
 
 describe "Docking Station" do
- 	let(:bike) {double :bike}
- 	let(:station) { DockingStation.new(capacity: 20)}
 
-it 'should accept all bikes' do
-	expect(station.bike_count).to eq(0)
-	station.accept(bike)
-	expect(station.bike_count).to eq(1)
-end
+	let(:bike) {double :bike}
+	let(:broken_bike) {double :bike, broken?: true}
+	let(:working_bike) {double :bike, broken?: false}
+	let(:container) {DummyContainer.new}
 
-it 'should release a broken bike' do
-	broken_bike = double :bike, broken?: true
-	station.accept(broken_bike)
-	expect(station.bike_count).to eq(1)
-	station.release_broken_bikes
-	expect(station.bike_count).to eq(0)
-end
+	it 'docks bikes' do
+		expect(container.bikes.count).to eq(0)
+		container.accept(bike)
+		expect(container.bikes.count).to eq(1)
+	end
 
-it 'should release a working bike' do
-	working_bike = double :bike, broken?: false
-	station.accept(working_bike)
-	expect(station.bike_count).to eq(1)
-	station.release_working_bikes
-	expect(station.bike_count).to eq(0)
-end
+	it 'releases bikes' do
+		expect(container.bikes.count).to eq(0)
+		container.accept(bike)
+		expect(container.bikes.count).to eq(1)
+		container.release(bike)
+		expect(container.bikes.count).to eq(0)
+	end
 
-it "should know when it's reached full capacity" do
-	expect(station).not_to be_full
-	20.times { station.accept(bike) }
-	expect(station).to be_full
-end
+	it 'can reach full capacity' do
+		expect(container).not_to be_full
+		20.times { container.accept(bike) }
+		expect(container).to be_full
+	end
 
-it "should not accept a bike if it's reached full capacity" do
-	20.times { station.accept(bike) }
-	expect(lambda { station.accept(bike) }).to raise_error(RuntimeError)
-end
+	it 'raises an error when it reaches full capacity' do
+		expect(container.bikes.count).to eq(0)
+		20.times { container.accept(bike) }
+		expect(lambda { container.accept(bike) }).to raise_error(RuntimeError)
+	end
 
-it 'should provide a list of working bikes' do
-	working_bike = double :bike, broken?: false
-	broken_bike = double :bike, broken?: true
-	station.accept(working_bike)
-	station.accept(broken_bike)
-	expect(station.working_bikes).to eq([working_bike])
-end
+	it 'segregates broken bikes' do
+		container.accept(broken_bike)
+		container.accept(working_bike)
+		expect(container.broken_bikes).to eq([broken_bike])
+	end
 
-it 'should provide a list of broken bikes' do
-	working_bike = double :bike, broken?: false
-	broken_bike = double :bike, broken?: true
-	station.accept(working_bike)
-	station.accept(broken_bike)
-	expect(station.broken_bikes).to eq([broken_bike])
-end
+	it 'segregates working bikes' do
+		container.accept(broken_bike)
+		container.accept(working_bike)
+		expect(container.working_bikes).to eq([working_bike])
+	end
+
+#  	let(:bike) {double :bike}
+#  	let(:station) { DockingStation.new(capacity: 20)}
+
+# it 'should accept all bikes' do
+# 	expect(station.bike.count).to eq(0)
+# 	station.accept(bike)
+# 	expect(station.bike.count).to eq(1)
+# end
+
+# it 'should release a broken bike' do
+# 	broken_bike = double :bike, broken?: true
+# 	station.accept(broken_bike)
+# 	expect(station.bike.count).to eq(1)
+# 	station.release_broken_bikes
+# 	expect(station.bike.count).to eq(0)
+# end
+
+# it 'should release a working bike' do
+# 	working_bike = double :bike, broken?: false
+# 	station.accept(working_bike)
+# 	expect(station.bike.count).to eq(1)
+# 	station.release_working_bikes
+# 	expect(station.bike.count).to eq(0)
+# end
+
+# it "should know when it's reached full capacity" do
+# 	expect(station).not_to be_full
+# 	20.times { station.accept(bike) }
+# 	expect(station).to be_full
+# end
+
+# it "should not accept a bike if it's reached full capacity" do
+# 	20.times { station.accept(bike) }
+# 	expect(lambda { station.accept(bike) }).to raise_error(RuntimeError)
+# end
+
+# it 'should provide a list of working bikes' do
+# 	working_bike = double :bike, broken?: false
+# 	broken_bike = double :bike, broken?: true
+# 	station.accept(working_bike)
+# 	station.accept(broken_bike)
+# 	expect(station.working_bikes).to eq([working_bike])
+# end
+
+# it 'should provide a list of broken bikes' do
+# 	working_bike = double :bike, broken?: false
+# 	broken_bike = double :bike, broken?: true
+# 	station.accept(working_bike)
+# 	station.accept(broken_bike)
+# 	expect(station.broken_bikes).to eq([broken_bike])
+# end
 
 end
